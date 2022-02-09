131157c18b2012ab2543e2ad8285b2a9335786fa
ggomaeng <sungwoopark95@gmail.com>
2022-02-09 15:09:03 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Landing from "../components/Landing";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Landing />
+    </div>
+  );
+}
